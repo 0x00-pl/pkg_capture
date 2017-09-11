1c5d1ab6a97756c4e0457b6a11db91bc5639bce5
pl <0x00.pl@gmail.com>
2017-09-11 17:34:29 +0800

    commit code

---
+++ build/CMakeFiles/pkg_capture.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pkg_capture.dir/sniffex.cpp.o"
+  "pkg_capture.pdb"
+  "pkg_capture"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pkg_capture.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
